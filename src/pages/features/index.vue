0b2e12f55f396b6cfb65ce3d37a7be5bdad1454a
Stanley Gomes <stanleygomesdasilva@gmail.com>
2018-02-24 19:47:34 -0300

    correcao de rotas

--- src/pages/features/index.vue
+++ src/pages/features/index.vue
@@ -1,9 +1,6 @@
 <template>
   <q-layout class="q-layout-page row justify-center">
     <q-toolbar color="primary" class="toolbar-height">
-        <router-link to="/apps">
-            <q-btn flat round dense icon="keyboard_arrow_left" color="white" />
-        </router-link>
       <q-toolbar-title><strong>{{ title }}</strong></q-toolbar-title>
       <router-link to="/">
         <q-btn flat round dense icon="power_settings_new" color="white" />
@@ -12,24 +9,31 @@
     <div class="options text-center">
       <q-card inline class="content row bigger q-ma-sm text-center">
         <q-list class="q-list q-list-separator q-list-highlight">
-          <router-link to="/features/ID">
+          <router-link to="/apps/ID">
             <q-item class="padding-v-30 cursor-pointer">
               <q-item-main class="text-center">
-                <q-item-tile label>FUNCIONALIDADES</q-item-tile>
+                <q-item-tile label>CASHLINK</q-item-tile>
               </q-item-main>
             </q-item>
           </router-link>
-          <router-link to="/profiles/ID">
+          <router-link to="/apps/ID">
             <q-item class="padding-v-30 cursor-pointer">
               <q-item-main class="text-center">
-                <q-item-tile label>PERFIS</q-item-tile>
+                <q-item-tile label>INDÚSTRA</q-item-tile>
               </q-item-main>
             </q-item>
           </router-link>
-          <router-link to="/users/ID">
+          <router-link to="/apps/ID">
             <q-item class="padding-v-30 cursor-pointer">
               <q-item-main class="text-center">
-                <q-item-tile label>USUÁRIOS</q-item-tile>
+                <q-item-tile label>SOU24H</q-item-tile>
+              </q-item-main>
+            </q-item>
+          </router-link>
+          <router-link to="/apps/ID">
+            <q-item class="padding-v-30 cursor-pointer">
+              <q-item-main class="text-center">
+                <q-item-tile label>TOBIAS</q-item-tile>
               </q-item-main>
             </q-item>
           </router-link>
@@ -46,11 +50,11 @@
 import { openURL, QField, QInput, QCard, QItem, QList, QItemTile, QToolbar } from 'quasar'
 
 export default {
-  name: 'detail',
+  name: '',
   data () {
     return {
       leftDrawerOpen: false,
-      title: 'CASHLINK'
+      title: 'FUNCIONALIDADES'
     }
   },
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,18 +2,25 @@
 export default [
   {path: '/', component: () => import('layouts/default')},
 
-  {
-    path: '/apps',
-    component: () => import('pages/apps/index'),
-    children: [
-      { path: 'create', component: () => import('pages/apps/create') },
-      { path: 'edit', component: () => import('pages/apps/edit') },
-      { path: 'ID', component: () => import('pages/apps/detail') }
-    ]
-  },
+  { path: '/apps', component: () => import('pages/apps/index') },
+  { path: '/apps/create', component: () => import('pages/apps/create') },
+  { path: '/apps/edit', component: () => import('pages/apps/edit') },
+  { path: '/apps/ID', component: () => import('pages/apps/detail') },
 
-  {path: '/permissions', component: () => import('pages/listaPermissoes')},
-  {path: '/register', component: () => import('pages/cadastroPerfil')},
+  { path: '/features', component: () => import('pages/features/index') },
+  // { path: '/features/create', component: () => import('pages/features/create') },
+  // { path: '/features/edit', component: () => import('pages/features/edit') },
+  // { path: '/features/ID', component: () => import('pages/features/detail') },
+
+  // {
+  //   path: '/apps',
+  //   component: () => import('pages/apps/index'),
+  //   children: [
+  //     { path: '/apps/create', component: () => import('pages/apps') },
+  //     { path: 'edit', component: () => import('pages/apps/edit') },
+  //     { path: 'ID', component: () => import('pages/apps/detail') }
+  //   ]
+  // },
 
   { // Always leave this as last one
     path: '*',
