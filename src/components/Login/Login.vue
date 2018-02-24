9bf73608fe5dbba29ce5a6eb2e470c99893df94e
Caio <caio.rod18@gmail.com>
2018-02-24 17:16:22 -0300

    tela login

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,25 @@
+<template>
+<q-card>
+  <q-card-title>
+    Card Title
+  </q-card-title>
+  <q-card-main>
+    Card Content
+  </q-card-main>
+</q-card>
+  
+</template>
+
+<script>
+import { QCard, QCardTitle } from 'quasar'
+name: 'LayoutDefault'
+export default {
+    data () {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,63 +1,45 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-layout-header>
-      <q-toolbar color="primary" glossy>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        >
-          <q-icon name="menu" />
-        </q-btn>
-
-        <q-toolbar-title>
-          Universo 11
-          <div slot="subtitle">Sistema de Acessos</div>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-layout-header>
-
-    <q-layout-drawer
-      v-model="leftDrawerOpen"
-      content-class="bg-grey-2"
-    >
-      <q-list
-        no-border
-        link
-        inset-delimiter
-      >
-        <q-list-header>Menu Principal</q-list-header>
-        <q-item @click.native="openURL('http://quasar-framework.org')">
-          <q-item-side icon="school" />
-          <q-item-main label="Sistemas" />
-        </q-item>
-        <q-item @click.native="openURL('https://github.com/quasarframework/')">
-          <q-item-side icon="code" />
-          <q-item-main label="Perfis" />
-        </q-item>
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="chat" />
-          <q-item-main label="Acessos"/>
-        </q-item>
-      </q-list>
-    </q-layout-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+    <q-layout view="lHh Lpr lFf">
+      <div>
+      <q-card class="card-style">
+      <q-card-title align="center">
+        {{title}}
+      </q-card-title>
+        <q-card-separator />
+        <q-card-main>
+        <q-field icon="person">
+          <q-input v-model="email" float-label="Usuario" type="email" suffix="" />
+        </q-field>
+        <br>
+        <q-field icon="lock">
+          <q-input v-model="email" float-label="Senha" type="email" suffix="" />
+        </q-field>
+        </q-card-main>
+        <q-btn class="text-center" align="center" label="Login" />
+      </q-card>
+      </div>
+    </q-layout>
 </template>
-
 <script>
-import { openURL } from 'quasar'
+import { openURL, QField, QInput, QCardTitle, QCard, QCardMain, QCardSeparator, QButton } from 'quasar'
 
 export default {
   name: 'LayoutDefault',
   data () {
     return {
-      leftDrawerOpen: false
+      email: '',
+      title: 'Login'
     }
+  },
+
+  components: {
+    QField,
+    QInput,
+    QCard,
+    QCardTitle,
+    QCardMain,
+    QCardSeparator,
+    QButton
   },
   methods: {
     openURL
@@ -66,4 +48,8 @@ export default {
 </script>
 
 <style>
+.card-style {
+  width: 20vw;
+}
+
 </style>
